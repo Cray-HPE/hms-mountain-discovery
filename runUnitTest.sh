6ebb507a04e2f59bf4188787b95a50e8348ec91d
Mitchell Schooler <mitchell.schooler@hpe.com>
2022-07-28 18:55:22 -0500

    Fix and update runUnitTest.sh.

--- runUnitTest.sh
+++ runUnitTest.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # MIT License
-
-# (C) Copyright [2021] Hewlett Packard Enterprise Development LP
-
+#
+# (C) Copyright 2021-2022 Hewlett Packard Enterprise Development LP
+#
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
 # to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense,
 # and/or sell copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following conditions:
-
+#
 # The above copyright notice and this permission notice shall be included
 # in all copies or substantial portions of the Software.
-
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -22,30 +22,46 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-# It's possible we don't have docker-compose, so if necessary bring our own.
-docker_compose_exe=$(command -v docker-compose)
-if ! [[ -x "$docker_compose_exe" ]]; then
-    if ! [[ -x "./docker-compose" ]]; then
-        echo "Getting docker-compose..."
-        curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" \
-        -o ./docker-compose
+set -x
 
-        if [[ $? -ne 0 ]]; then
-            echo "Failed to fetch docker-compose!"
-            exit 1
-        fi
-
-        chmod +x docker-compose
-    fi
-    docker_compose_exe="./docker-compose"
+# Add .exe if running in a WSL environment
+if $(uname -r | grep -q "Microsoft"); then
+    shopt -s expand_aliases
+    alias docker-compose=docker-compose.exe
 fi
 
-# Get the containers running, build them, and exit on any of them exiting
-echo "Starting containers... running test"
-${docker_compose_exe} -f docker-compose.yml up  --build --abort-on-container-exit
+# Configure docker compose
+export COMPOSE_PROJECT_NAME=$RANDOM
+export COMPOSE_FILE=docker-compose.yml
+
+args="-f $COMPOSE_FILE -p $COMPOSE_PROJECT_NAME"
+
+echo "COMPOSE_PROJECT_NAME: ${COMPOSE_PROJECT_NAME}"
+echo "COMPOSE_FILE: $COMPOSE_FILE"
+
+function cleanup() {
+  docker-compose $args down
+  if [[ $? -ne 0 ]]; then
+    echo "Failed to decompose environment!"
+    exit 1
+  fi
+  exit $1
+}
+
+
+# Get the base containers running
+echo "Starting containers..."
+docker-compose $args build
+docker-compose $args up --exit-code-from rediscovery rediscovery
+
 test_result=$?
+
 # Clean up
 echo "Cleaning up containers..."
-${docker_compose_exe} down
+if [[ $test_result -ne 0 ]]; then
+  echo "Unit tests FAILED!"
+  cleanup 1
+fi
 
-exit ${test_result}
+echo "Unit tests PASSED!"
+cleanup 0
